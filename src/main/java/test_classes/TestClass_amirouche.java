a5eb00a90d60e0948f149f3aae6ac56427b17101
amirouche zeggagh <amirouchez656@gmail.com>
2018-11-21 23:29:30 +0100

    fix number of foreign key

--- src/main/java/test_classes/TestClass_amirouche.java
+++ src/main/java/test_classes/TestClass_amirouche.java
@@ -11,6 +11,8 @@ import db.connection.SQLConnection;
 import db.save_and_load.SaveProject;
 import db.validation.PreCondetion;
 import static db.validation.PreCondetion.CHECKED_TRUE;
+import java.util.Arrays;
+import java.util.List;
 import javafx.scene.control.TitledPane;
 
 /**
@@ -18,16 +20,27 @@ import javafx.scene.control.TitledPane;
  * @author amirouche
  */
 public class TestClass_amirouche {
-    SQLSchema sqlSchema ;
+
+    SQLSchema sqlSchema;
+
     public void main() throws Exception {
-        
-        SQLConnection cnx = new SQLConnection("/home/amirouche/NetBeansProjects/MASAMA/mySQL/test.sql", "sqlite", false);
+        //le clé prémair sont il possible de generé pour les chane
+        SQLConnection cnx = new SQLConnection("/home/amirouche/NetBeansProjects/MASAMA/mySQL/test.sql", "SQLite", false);
         sqlSchema = new SQLSchema();
+
+        sqlSchema.getTable("InfoA").setHowMuch(3);
+        sqlSchema.getTable("InfoB").setHowMuch(2);
+        sqlSchema.getTable("InfoC").setHowMuch(7);
+
+        sqlSchema.startToGenerateInstances();
+
+        for (Table table : sqlSchema.getTables()) {
+            table.show();
+        }
         //testPrecondetion();
-        testSaveProject();   
+        //testSaveProject();
     }
-    
-    
+
     private void testSaveProject() {
         //j'ai decomenté la method toString de Attribure
         /*
@@ -36,39 +49,42 @@ public class TestClass_amirouche {
         *
          j'ai suprimmer de la class Table
         *   private JFXTreeTableView<AttributeModel> tableView;
-        *   private SwingNode insertsView = null;        
+        *   private SwingNode insertsView = null;
         *
          j'ai suprémir (commenté)quellque line dans dans MainController
         * cherche sur delete amirouche
         *
         *
-        */
-        
-        
+         */
+
         sqlSchema.startToGenerateInstances();
-        SaveProject saveProject=new SaveProject(sqlSchema);
-        String path="/home/amirouche/NetBeansProjects/masama_generator/SaveProject";
+        SaveProject saveProject = new SaveProject(sqlSchema);
+        String path = "/home/amirouche/NetBeansProjects/masama_generator/SaveProject";
         saveProject.saveSQLSchema(path);
-        
+
     }
-    
+
     private void testPrecondetion() throws Exception {
-                
-        //sqlSchema.getTable("Movie").setHowMuch(9);
-        //sqlSchema.getTable("Rating").setHowMuch(10);
-       
-        PreCondetion preCondetion=new PreCondetion(sqlSchema);                
-        String msgCheck= preCondetion.checkSqlScema();        
-        if (msgCheck.equals(CHECKED_TRUE)) {            
+
+        sqlSchema.getTable("InfoA").setHowMuch(1);
+        sqlSchema.getTable("InfoB").setHowMuch(5);
+        sqlSchema.getTable("InfoC").setHowMuch(4);
+
+        sqlSchema.startToGenerateInstances();
+
+        /*
+        PreCondetion preCondetion=new PreCondetion(sqlSchema);
+        String msgCheck= preCondetion.checkSqlScema();
+        if (msgCheck.equals(CHECKED_TRUE)) {
             System.out.println("we can generate");
             sqlSchema.startToGenerateInstances();
-            for (Table table : sqlSchema.getTables()) {                
+            for (Table table : sqlSchema.getTables()) {
                 table.show();
             }
-            
+
         }else{
             System.out.println(msgCheck);
             //showAlertDialogue whith mesage is mesageCheck
-        }
+        }*/
     }
 }

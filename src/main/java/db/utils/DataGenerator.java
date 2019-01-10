3d5f1d63dbced201f63c32abec8d352b48220714
saadchiadmi <saadchiadmi@gmail.com>
2019-01-10 12:15:53 +0100

    generator unique pour int

--- src/main/java/db/utils/DataGenerator.java
+++ src/main/java/db/utils/DataGenerator.java
@@ -46,33 +46,29 @@ public final class DataGenerator {
         //System.out.println("list begin: " + preDate);
         Random random = new Random();
         List<String> result = new ArrayList<>();
-        for (int i = 1; i < 10; i++) {
-            result = new ArrayList<>();
-            int jumper = i;//random.nextInt(to - from);
-            System.out.println("jumper :" + jumper);
-            if (preDate.size() > size) {
-                result = preDate.stream().limit(size).collect(Collectors.toList());
-            } else {
-                result.addAll(preDate);
-                int j = from;
-                while (result.size() < size) {
-                    j += jumper;
-                    j = (j > to) ? j % to : j;
-                    System.out.println("j :" + j);
-                    if (!result.contains(String.valueOf(j))) {
-                        j = (j < from) ? j + from : j;
-                        if (!result.contains(String.valueOf(j))) {
-                            result.add(String.valueOf(j));
-                        }
-                        jumper--;
-                        jumper = (jumper == 0) ? to - from : jumper;
-                        System.out.println("jumper :" + jumper);
-                    }
+        int jumper = random.nextInt(to - from);
+        System.out.println("jumper1 :" + jumper);
+        if (preDate.size() > size) {
+            result = preDate.stream().limit(size).collect(Collectors.toList());
+        } else {
+            result.addAll(preDate);
+            int j = from;
+            while (result.size() < size) {
+                jumper = (jumper == 0) ? to - 1 : jumper;
+                jumper = (jumper == (from)/2) ? jumper - 1 : jumper;
+                j += jumper;
+                System.out.println("j :" + j);
+                j = (j > to) ? j % to : j;
+                System.out.println("j 2:" + j);
+                j = (j < from) ? j + from : j;
+                System.out.println("j 3:" + j);
+                if (!result.contains(String.valueOf(j))) {
+                    System.out.println("ajouter" + j);
+                    result.add(String.valueOf(j));
                 }
             }
-            System.out.println(result);
         }
-        //System.out.println("list end: " + result);
+        System.out.println("list end: " + result);
         return result;
     }
 
